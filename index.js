c13f5c67a05413883d0d9f090d29fbac2c462b8a
Kaz <kazimier.rubel27@student.tamdistrict.org>
2025-05-29 18:04:41 -0700

    added Random button and basic css

--- index.js
+++ index.js
@@ -16,10 +16,30 @@ async function getData(query) {
         console.error(error.message);
     }
 }
+async function getRandomMovie() {
+    const url = "https://api.themoviedb.org/3/movie/popular?page=" + Math.floor(Math.random() * 20 + 1);
+    try {
+        const response = await fetch(url, {
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTJiMTQ2MjM0NDM4ODI1YmI4MmFmYTZkMmYxNmQ5MCIsIm5iZiI6MTc0Nzc1NjM1OS42MTksInN1YiI6IjY4MmNhNTQ3OWMzOTJjMWU2MWY1NjQxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bf_LqZV9-xm47RN742pMgMDNXy7k248VfkQeYIFham4",
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        const results = json.results;
+        return results[Math.floor(Math.random() * results.length)];
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 const searchInputEl = document.getElementById("search-input");
 const searchButtonEl = document.getElementById("search-button");
 const searchResultsEl = document.getElementById("search-results");
 const randomButtonEl = document.getElementById("random-button");
+const randomResultsEl = document.getElementById("random-results");
 
 
 
@@ -42,3 +62,17 @@ searchButtonEl.addEventListener("click", async function (event) {
         }
     }
 });
+randomButtonEl.addEventListener("click", async function (event) {
+    const randomMovie = await getRandomMovie();
+    randomResultsEl.innerHTML = "";
+    const resultEl = document.createElement("div");
+    const imgEl = document.createElement("img");
+    imgEl.src = "https://image.tmdb.org/t/p/original/" + randomMovie.poster_path;
+    imgEl.width = "100";
+
+    resultEl.textContent = randomMovie.title + "Genre" + randomMovie["genre_ids"].map(id => genres[id]).join(", ")
+    resultEl.appendChild(imgEl);
+    randomResultsEl.appendChild(resultEl);
+});
+
+
