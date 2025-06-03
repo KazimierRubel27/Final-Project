8d44a0d66be6e8730d4a0da44b5409592e39a6da
Kaz <kazimier.rubel27@student.tamdistrict.org>
2025-06-03 09:51:39 -0700

    List by genre (not working yet)

--- index.js
+++ index.js
@@ -35,12 +35,41 @@ async function getRandomMovie() {
         console.error(error.message);
     }
 }
+async function getMovieByGenre(genre = 'action') {
+    let found_genre_id = -1
+    for (const [genre_id, genre_name] of Object.entries(genres)) {
+        if (genre == genre_name) {
+            found_genre_id = genre_id;
+        }
+    }
+    // found_genre_id = Object.entries(genres).find(([k,v]) => v == genre)[0]
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${found_genre_id}`;
+    try {
+        const response = await fetch(url, {
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTJiMTQ2MjM0NDM4ODI1YmI4MmFmYTZkMmYxNmQ5MCIsIm5iZiI6MTc0Nzc1NjM1OS42MTksInN1YiI6IjY4MmNhNTQ3OWMzOTJjMWU2MWY1NjQxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bf_LqZV9-xm47RN742pMgMDNXy7k248VfkQeYIFham4",
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        const results = json.results;
+        const randomInt = Math.abs(Math.random() * results.length - 20);
+        return results.slice(randomInt, randomInt + 20);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
 const searchInputEl = document.getElementById("search-input");
 const searchButtonEl = document.getElementById("search-button");
 const searchResultsEl = document.getElementById("search-results");
 const randomButtonEl = document.getElementById("random-button");
 const randomResultsEl = document.getElementById("random-results");
-
+const genreInputEl = document.getElementById("genre-input");
+const genreButtonEl = document.getElementById("genre-button");
 
 
 searchButtonEl.addEventListener("click", async function (event) {
@@ -76,5 +105,21 @@ randomButtonEl.addEventListener("click", async function (event) {
     resultEl.appendChild(imgEl);
     randomResultsEl.appendChild(resultEl);
 });
-
-
+genreButtonEl.addEventListener("click", async function (event) {
+    const rand = await getMovieByGenre(genreInputEl.value)
+    if (rand.length == 0) {
+        console.log("No movies in that genre")
+    } else {
+        console.log(rand)
+        searchResultsEl.innerHTML = "";
+        randomResultsEl.innerHTML = "";
+        for (const result of rand) {
+            const resultEl = document.createElement("div");
+            const imgEl = document.createElement("img");
+            imgEl.src = "https://image.tmdb.org/t/p/original/" + result.poster_path;
+            resultEl.textContent = result.title + " Genres: " + result["genre_ids"].map(id => genres[id]).join(", ");
+            resultEl.appendChild(imgEl);
+            searchResultsEl.appendChild(resultEl);
+        }
+    }
+});
